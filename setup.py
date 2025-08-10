0a949cb36b3e078f06934bb280ff0533cad8e7be
Diego <df_132009@hotmail.com>
2025-08-10 12:08:45 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Reporelease",                             # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Diego Cupitra",                         # Tu nombre
+    author_email="df_132009@hotmail.com",           # Tu correo electrónico
+    url="https://github.com/diegocupitra/reporelease",     # URL del proyecto
+)
